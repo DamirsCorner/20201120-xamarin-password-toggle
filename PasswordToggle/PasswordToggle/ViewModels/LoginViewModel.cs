3af347360f41ccb531a78905205c0b35879b2658
Damir Arh <damir.arh@gmail.com>
2020-11-01 10:49:53 +0100

    Implementation in a standalone control

--- PasswordToggle/PasswordToggle/ViewModels/LoginViewModel.cs
+++ PasswordToggle/PasswordToggle/ViewModels/LoginViewModel.cs
@@ -11,11 +11,8 @@ namespace PasswordToggle.ViewModels
     {
         private string username;
         private string password;
-        private bool hidePassword = true;
 
         public Command LoginCommand { get; }
-
-        public Command TogglePasswordCommand { get; }
 
         public string Username
         {
@@ -29,27 +26,15 @@ namespace PasswordToggle.ViewModels
             set => SetProperty(ref password, value); 
         }
 
-        public bool HidePassword
-        {
-            get => hidePassword;
-            private set => SetProperty(ref hidePassword, value);
-        }
-
         public LoginViewModel()
         {
             LoginCommand = new Command(OnLoginClicked);
-            TogglePasswordCommand = new Command(OnTogglePasswordClicked);
         }
 
         private async void OnLoginClicked(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
             await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
-        }
-
-        private void OnTogglePasswordClicked()
-        {
-            HidePassword = !HidePassword;
         }
     }
 }
